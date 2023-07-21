6105c0140f6ad27ee6243cc53a72bddaac19f526
pc215 <chouksepranav@gmail.com>
2023-07-21 22:55:44 +0100

    Add template files for todo

---
+++ todo.h
@@ -0,0 +1,19 @@
+/* Specification:
+ * ADD: Add a new task (with importance and urgency values)
+ * DELETE: Remove and unwanted task
+ * COMPLETE: Complete a task and add to another list (then delete it)
+ * COMPLETE_TOP_TASK: A special command using the COMPLETE function
+ * CHANGE URGENCY: Change urgency of the task and re-order
+ * CHANGE IMPORTANCE: Change importance of the task and re-order
+ */
+
+// To make it future-proof adding the list as an argument
+
+/**
+ * Add a new task to the todo list
+ * @param list - The todo list
+ * @param task - The task to be added
+ * @param importance - The importance of the task (from 0-9)
+ * @param urgency - The urgency of the task (from 0-9)
+ */
+
